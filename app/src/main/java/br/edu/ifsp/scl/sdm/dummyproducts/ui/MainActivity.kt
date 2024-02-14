232ab94220ff5877fda2a90d5b02f89c940ddb34
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2024-02-14 20:57:11 -0300

    Create adapter fo product

--- app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
@@ -3,12 +3,19 @@ package br.edu.ifsp.scl.sdm.dummyproducts.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.edu.ifsp.scl.sdm.dummyproducts.R
+import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductAdapter
 import br.edu.ifsp.scl.sdm.dummyproducts.databinding.ActivityMainBinding
+import br.edu.ifsp.scl.sdm.dummyproducts.model.Product
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private val productList: MutableList<Product> = mutableListOf()
+    private val productAdapter: ProductAdapter by lazy {
+        ProductAdapter(this, productList)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
@@ -16,5 +23,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(amb.mainTb.apply {
             title = getString(R.string.app_name)
         })
+
+        amb.productsSp.adapter = productAdapter
     }
 }
