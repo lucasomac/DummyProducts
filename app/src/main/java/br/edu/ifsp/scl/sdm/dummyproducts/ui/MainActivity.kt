0c42e70a44205a4a6fb7984b4eb08601b137f1ca
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2024-02-14 21:29:29 -0300

    Finalized list of products get

--- app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
@@ -2,10 +2,21 @@ package br.edu.ifsp.scl.sdm.dummyproducts.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.edu.ifsp.scl.sdm.dummyproducts.R
 import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductAdapter
 import br.edu.ifsp.scl.sdm.dummyproducts.databinding.ActivityMainBinding
 import br.edu.ifsp.scl.sdm.dummyproducts.model.Product
+import br.edu.ifsp.scl.sdm.dummyproducts.model.ProductList
+import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
+import org.json.JSONException
+import java.io.IOError
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.HttpURLConnection.HTTP_OK
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
@@ -25,5 +36,43 @@ class MainActivity : AppCompatActivity() {
         })
 
         amb.productsSp.adapter = productAdapter
+        retrieveProducts()
+    }
+
+    private fun retrieveProducts() =
+        Thread {
+            val productsConnection = URL(PRODUCTS_ENDPOINT).openConnection() as HttpURLConnection
+            try {
+                if (productsConnection.responseCode == HTTP_OK) {
+                    InputStreamReader(productsConnection.inputStream).readText().let {
+                        runOnUiThread {
+                            productAdapter.addAll(Gson().fromJson(it, ProductList::class.java).products)
+                        }
+                    }
+                } else {
+                    Toast.makeText(
+                        this, getString(R.string.message_request_problem), Toast.LENGTH_SHORT
+                    ).show()
+                }
+            } catch (ioe: IOException) {
+                runOnUiThread {
+                    Toast.makeText(
+                        this, getString(R.string.message_connection_failed), Toast.LENGTH_SHORT
+                    ).show()  }
+
+            } catch (jse: JsonSyntaxException) {
+                runOnUiThread{
+                    Toast.makeText(
+                        this, getString(R.string.message_response_problem), Toast.LENGTH_SHORT
+                    ).show()
+                }
+            } finally {
+                productsConnection.disconnect()
+            }
+        }.start()
+
+
+    companion object {
+        const val PRODUCTS_ENDPOINT = "https://dummyjson.com/products/"
     }
 }
